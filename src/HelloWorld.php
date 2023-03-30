7c9b727848e55f3ba44d2f855d7fec9050229bd3
sautmanurung1 <smanurung360@gmail.com>
2023-03-31 01:32:03 +0700

    new function

--- src/HelloWorld.php
+++ src/HelloWorld.php
@@ -7,4 +7,8 @@ class HelloWorld {
     public function sayHello($name) {
         return "Hello $name welcome to PHP and Composer";
     }
+
+    public function seyBye($name) {
+        return "BYE $name nice to meet you, see you again";
+    }
 }
